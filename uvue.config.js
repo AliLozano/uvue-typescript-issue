5494537dd078b265fbad92ab1df142452beb2c1c
Ali Lozano <alilozanoc@gmail.com>
2019-08-26 23:53:56 -0500

    Circular structure to JSON issue

---
+++ uvue.config.js
@@ -0,0 +1,8 @@
+export default {
+  plugins: [
+    '@uvue/core/plugins/asyncData',
+    '@uvue/core/plugins/vuex',
+    '@uvue/core/plugins/middlewares',
+    '@uvue/core/plugins/errorHandler',
+  ],
+};
